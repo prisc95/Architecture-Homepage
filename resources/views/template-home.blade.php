a613c3ce50b6b6db8ab8093171b94f38a2e4f9e9
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-15 11:27:46 +0200

    Setup Project

---
+++ resources/views/template-home.blade.php
@@ -0,0 +1,17 @@
+{{--
+  Template Name: Home Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+    {{-- @include('partials.page-header')
+    @include('partials.content-page') --}}
+    @include('partials.pageblocks.hero')
+    {{-- @include('partials.pageblocks.about')
+    @include('partials.pageblocks.projects')
+    @include('partials.pageblocks.customers')
+    @include('partials.pageblocks.contact') --}}
+  @endwhile
+@endsection
