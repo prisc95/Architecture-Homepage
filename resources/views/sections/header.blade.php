93b661753eac41388496f6b5c4aee3ba254e8b02
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-15 16:21:59 +0200

    Created the navbar with interactive animation on it

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,3 +1,6 @@
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 <header class="banner">
   <a class="brand" href="{{ home_url('/') }}">
     {{-- {!! $siteName !!} --}}
@@ -7,27 +10,25 @@
   <section class="navbar">
 
     <div class="container">
-        <div class="logos">
-              <div class="logo1"></div>
-              <div class="logo2"></div>
-              <div class="logo3"></div>
-              <span>Architectuur</span>
+        <div class="logo">
+              <div class="img-wrapper">
+                <img src="wp-content/themes/sage/resources/images/architect-logo.png" alt="">
+              </div>
+              <h1>Architectuur</h1>
         </div>
 
+        <div class="right">
         <div class="searchbar">
               <input type="text" placeholder="Waar ben je naar op zoek?">
                 <i class="fa-solid fa-magnifying-glass"></i>
         </div>
 
-        <div class="burger-menu">
-
-            <div class="icon">
-                <div class="line line1"></div>
-                <div class="line line2"></div>
-                <div class="line line3"></div>
-            </div>
-
-            <div class="navigation">
+        <div id="menu-icon" class="burger-menu">
+                  <i class="fa-solid fa-bars" style="color: #888888;"></i>
+        </div>
+        </div>
+    </div>
+     <div class="navigation">
                 <a href="#">HOME</a>
                 <a href="#">ABOUT</a>
                 <a href="#">PROJECTS</a>
@@ -35,9 +36,6 @@
                 <a href="#">CUSTOMERS</a>
                 <a href="#">CONTACT</a>
             </div>
-            
-        </div>
-    </div>
   </section>
 
 
