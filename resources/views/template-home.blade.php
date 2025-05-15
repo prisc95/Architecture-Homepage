ae1ffba4f2dfa9e847ab321635243a0b3e487d45
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-15 12:07:36 +0200

    Updated home-template, variables.scss, base.scss

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -9,8 +9,8 @@
     {{-- @include('partials.page-header')
     @include('partials.content-page') --}}
     @include('partials.pageblocks.hero')
-    {{-- @include('partials.pageblocks.about')
-    @include('partials.pageblocks.projects')
+    @include('partials.pageblocks.about')
+    {{-- @include('partials.pageblocks.projects')
     @include('partials.pageblocks.customers')
     @include('partials.pageblocks.contact') --}}
   @endwhile
