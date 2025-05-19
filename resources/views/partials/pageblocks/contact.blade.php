744f37655e32e6260e4bbfe72ed087700c1f7d3f
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-19 10:56:48 +0200

    Updated contact & footer section to be responsive on all screen sizes

--- resources/views/partials/pageblocks/contact.blade.php
+++ resources/views/partials/pageblocks/contact.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 <section class="contact">
 
     <div class="container">
@@ -8,7 +10,7 @@
 
             <div class="right">
                 <input type="email" placeholder="E-mailadres">
-                    <i class="fa-solid fa-paper-plane"></i>
+                <i class="fa-solid fa-paper-plane" style="color: #ffffff;"></i>
             </div>
             
         </div>
--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -2,16 +2,18 @@
   @php(dynamic_sidebar('sidebar-footer'))
 
   <div class="container">
-    <div class="logos">
-        <div class="logo1"></div>
-        <div class="logo2"></div>
-        <div class="logo3"></div>
-        <span>Architectuur</span>
+    <div class="logo">
+        <div class="img-wrapper">
+                <img src="wp-content/themes/sage/resources/images/architect-logo.png" alt="">
+              </div>
+        <h3>Architectuur</h3>
     </div>
 
     <div class="copyright">
-        <p>&copy; All Rights Reserved. Gemaakt door Priscilla de Groot.</p>
+        <p>&copy; All Rights Reserved. Made by Priscilla de Groot.</p>
     </div>
+
+    <div></div>
     
   </div>
 
